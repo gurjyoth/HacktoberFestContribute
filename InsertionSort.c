5c8b95a9406c73194b1e2314cdf9ab3fb7aa8b8e
Gurjyoth Singh <161277696+gurjyoth@users.noreply.github.com>
2024-10-19 06:43:24 +0530

    shorted InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,27 +1,34 @@
-#include<stdio.h>
-int main()
-{
-	int i,n,A[100],j;
-	printf("Enter size of array:");
-	scanf("%d",&n);
-	printf("Enter %d elements:",n);
-	for(i=0;i<n;i++)
-		scanf("%d",&A[i]);
-	for (i=1;i<(n-1);i++)
-	{	
-		j=i;
-		while(j>0 && A[j+1]>A[j])
-		{
-			int t = A[j];
-			A[j] = A[j-1];
-			A[j-1] = t;
-			j--;
-		}
-	}
-	printf("Sorted array in ascending order:");
-	for(i = 0; i < n; i++)
-	{
-		printf("%d \n",A[i]);
-	}
-	return 0;
+#include <stdio.h>
+
+int main() {
+    int n, A[100];
+
+    // Input size and elements
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    // Insertion sort in descending order
+    for (int i = 1; i < n; i++) {
+        int key = A[i];
+        int j = i - 1;
+
+        // Move elements that are less than key to one position ahead
+        while (j >= 0 && A[j] < key) {
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[j + 1] = key; // Insert key at the correct position
+    }
+
+    // Output sorted array
+    printf("Sorted array in descending order:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", A[i]);
+    }
+
+    return 0;
 }
