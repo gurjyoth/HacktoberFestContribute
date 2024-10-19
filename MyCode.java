b3f43df4de99d52986d9b81426f368474aecb53f
Gurjyoth Singh <161277696+gurjyoth@users.noreply.github.com>
2024-10-19 06:51:03 +0530

    hangman-shorted-version

--- MyCode.java
+++ MyCode.java
@@ -1,123 +1,83 @@
 import java.util.Scanner;
 import java.util.Random;
-class Hangman
-{   
 
-    public static void main(String[] args)
-    {   
+class Hangman {
+    private static final String[] CITIES = {
+        "DELHI", "KARACHI", "NEWYORK", "LONDON", "TOKYO", 
+        "SURAT", "KABUL", "DUBAI", "JAIPUR", "PUNE"
+    };
+    private static final int MAX_LIVES = 10;
+
+    public static void main(String[] args) {
         System.out.println(" @@@@@@@@@@@@@ PLAY HANGMAN @@@@@@@@@@@@@" );
-        String[] s={"DELHI","KARACHI","NEWYORK","LONDON","TOKYO","SURAT","KABUL","DUBAI","JAIPUR","PUNE"};
-        Random r=new Random();
-        System.out.println("GUESS THE WORLD'S LARGEST CITY");
-        System.out.println("Rule: You have 10 chances for correct guess");
-        for(;;)
-        {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
 
-            int randomNumber=r.nextInt(s.length);
-            String sw=s[randomNumber];
-
-            String incorrect="";
-
-            String guess="";
-            Scanner sc=new Scanner(System.in);
-            
-            for(int x=0;x<sw.length();x++)
-            {
-                guess+="_";
-
-            }
-            int numLives=0;
-            for(;;)
-            {   
-
-                System.out.println("YOUR CORRECT GUESSES");
-                for(int x=0;x<sw.length();x++)
-                {
-                    char p=guess.charAt(x);
-                    System.out.print(p+" ");
-                }
-                System.out.println();
-                System.out.println("GUESS A LETTER");
-                String l=sc.nextLine().toUpperCase();
-                char c=l.charAt(0);
-                String ssw="";
-                if(incorrect.indexOf(c)!=-1)
-                    continue;
-                else if(guess.indexOf(c)!=-1)
-                    continue;
-                else if(incorrect.indexOf(c)==-1)
-                {
-                    if(sw.indexOf(c)!=-1 && guess.indexOf(c)==-1)
-
-                    {
-                        for(int x=0;x<sw.length();x++)
-                        {
-                            char a=sw.charAt(x);
-                            char b=guess.charAt(x);
-                            if(a==c)
-                            {
-                                ssw+=c;
-                            }
-                            else
-                            {
-                                ssw+=b;
-                            }
-                        }
-                        guess=ssw;
-                    }
-
-                    else
-                    {
-                        incorrect+=c;
-
-                        System.out.println("MISSES = "+incorrect);
-                        numLives++;
-                        
-                    }
-                }
-
-                if(guess.indexOf('_')==-1 )
-                    break;
-                if(numLives==10)
-                    break;
-            }
-            System.out.println();
-            if(guess.indexOf('_')!=-1) 
-            {System.out.println(sw);
-                System.out.println("@@ YOU HAVE LOST THE GAME @@ ");}
-            else
-            {System.out.print(sw);
-                System.out.println("@@ WON! CONGRATULATION GET YOUR PRICE @@");
-            }
-            String Q;
-            char P=0;
-            System.out.println("DO YOU WANT TO CONTINUE PLAY AGAIN IF YES THEN CLICK Y OR IF NOT THEN CLICK SOMETHING ELSE");
-            boolean repeat=false;
-            do
-            {
-                repeat=false;
-                try
-                {
-                    Q=sc.nextLine();
-                    P=Q.charAt(0);
-                    if(!Character.isLetter(P))
-                        throw new Exception();
-                }
-                catch (Exception e)
-                {
-                    repeat=true;
-                }
-            }
-            while(repeat==true);
-            if(P=='y'||P=='Y')
-                continue;
-            else
-            {
-                break;
-
-            }
-
+        while (true) {
+            String city = CITIES[random.nextInt(CITIES.length)];
+            playGame(sc, city);
+            if (!playAgain(sc)) break;
         }
+        sc.close();
+    }
 
+    private static void playGame(Scanner sc, String city) {
+        StringBuilder guessed = new StringBuilder("_".repeat(city.length()));
+        StringBuilder incorrect = new StringBuilder();
+        int lives = 0;
+
+        while (lives < MAX_LIVES && guessed.indexOf("_") != -1) {
+            displayProgress(guessed, incorrect);
+            char guess = getGuess(sc, incorrect);
+
+            if (city.indexOf(guess) != -1) {
+                updateGuessed(city, guessed, guess);
+            } else {
+                incorrect.append(guess);
+                lives++;
+                System.out.println("MISSES = " + incorrect);
+            }
+        }
+        
+        displayResult(city, guessed);
     }
+
+    private static void displayProgress(StringBuilder guessed, StringBuilder incorrect) {
+        System.out.println("YOUR CORRECT GUESSES: " + guessed);
+        System.out.println("GUESS A LETTER");
     }
+
+    private static char getGuess(Scanner sc, StringBuilder incorrect) {
+        char guess;
+        while (true) {
+            String input = sc.nextLine().toUpperCase();
+            guess = input.charAt(0);
+            if (Character.isLetter(guess) && incorrect.indexOf(String.valueOf(guess)) == -1) {
+                break;
+            }
+        }
+        return guess;
+    }
+
+    private static void updateGuessed(String city, StringBuilder guessed, char guess) {
+        for (int i = 0; i < city.length(); i++) {
+            if (city.charAt(i) == guess) {
+                guessed.setCharAt(i, guess);
+            }
+        }
+    }
+
+    private static void displayResult(String city, StringBuilder guessed) {
+        System.out.println();
+        if (guessed.indexOf("_") == -1) {
+            System.out.println(city + "\n@@ WON! CONGRATULATIONS, GET YOUR PRIZE @@");
+        } else {
+            System.out.println(city + "\n@@ YOU HAVE LOST THE GAME @@");
+        }
+    }
+
+    private static boolean playAgain(Scanner sc) {
+        System.out.println("DO YOU WANT TO PLAY AGAIN? (Y/N)");
+        return sc.nextLine().trim().equalsIgnoreCase("Y");
+    }
+}
