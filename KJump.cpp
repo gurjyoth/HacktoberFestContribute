e60ae7acd043d351708769f4c6f2ca036d9eca56
Gurjyoth Singh <161277696+gurjyoth@users.noreply.github.com>
2024-10-19 06:46:35 +0530

    Update KJump.cpp

--- KJump.cpp
+++ KJump.cpp
@@ -1,76 +1,49 @@
-// https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/long-jump-1-7d02705a/description/
-// Medium
-// Complexity : O(nLogn) try
-// better than O(n^2) but worst than O(nlogn)
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int bruteForce(vector<int>& A, int K) {
-    vector<int>len(A.size(), 1);
-    for(int i = 0; i < A.size(); i++) {
-        for(int j = i+1; j < A.size(); j++) {
-            if(A[j] - A[i] >= K) { len[j] = max(len[j], len[i]+1); }
-        }
-    }
-    int result = 0;
-    for(int x : len) result = max(result, x);
-    return result;
-}
-
-void binarySearch(int start, int end, vector<int>& maxLen, int X, int K)
-{
-    for(int i = start; i <= end; i++) {
-        if(X - maxLen[i] >= K) {
-            if(maxLen[i+1] >= X) {
-                maxLen[i+1] = X;
-                break;
+int binarySearch(int start, int end, vector<int>& maxLen, int value, int K) {
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (value - maxLen[mid] >= K) {
+            if (mid + 1 <= end && maxLen[mid + 1] >= value) {
+                maxLen[mid + 1] = value;
+                return mid + 1;
             }
+            start = mid + 1;
+        } else {
+            end = mid - 1;
         }
     }
+    return start;
 }
 
 int optimalSolution(vector<int>& arr, int K) {
+    vector<int> maxLen(arr.size(), INT_MAX);
+    int size = 0;
 
-    vector<int>maxLen(arr.size(), 0);
-    int size = -1;
-
-    for(int i = 0; i < arr.size(); i++) {
-        // If the value is the first element or valid greater than the last element
-        if(size == -1 || arr[i]-maxLen[size] >= K) {
-            maxLen[++size] = arr[i];
-        }
-        // If the value is smaller than the first element
-        else if(arr[i] <= maxLen[0]) { maxLen[0] = arr[i]; }
-        // Place the value somewhere in between O(logn) try
-        // worked with O(n)
-        else {
-            binarySearch(0, size, maxLen, arr[i], K);
+    for (int value : arr) {
+        if (size == 0 || value - maxLen[size - 1] >= K) {
+            maxLen[size++] = value;
+        } else {
+            int pos = binarySearch(0, size - 1, maxLen, value, K);
+            maxLen[pos] = value;
         }
     }
     
-    return size+1;
-}
-
-int KJump (vector<int>& A, int K) {
-    // Returns the length of longest subsequence
-    return optimalSolution(A, K);
+    return size;
 }
 
 int main() {
-
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int K;
-    cin >> K;
-    int N;
-    cin >> N;
+
+    int K, N;
+    cin >> K >> N;
     vector<int> A(N);
-    for(int i_A=0; i_A<N; i_A++) {
-    	cin >> A[i_A];
+    for (int& a : A) {
+        cin >> a;
     }
 
-    int out_;
-    out_ = KJump(A, K);
-    cout << out_;
+    cout << optimalSolution(A, K) << '\n';
+    return 0;
 }
