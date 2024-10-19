d60f79effac486128535da0ad57b41ac2f20a4e3
Gurjyoth Singh <161277696+gurjyoth@users.noreply.github.com>
2024-10-19 06:54:25 +0530

    shortened Menu Driven BST

--- MenuDrivenBST.c
+++ MenuDrivenBST.c
@@ -1,123 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct node
-{
-  int data;
-  struct node *left, *right;  
-}*root=NULL;
+struct node {
+    int data;
+    struct node *left, *right;  
+};
 
-struct node* insert(struct node* root, int ele)
-{
-  if(root == NULL)
-  {
-    root = (struct node*) malloc(sizeof(struct node));
-    root->data = ele;
-    root->left = NULL;
-    root->right = NULL;
-  }
-  if (ele < root->data) 
-        root->left  = insert(root->left, ele); 
+struct node* insert(struct node* root, int ele) {
+    if (!root) {
+        struct node* newNode = (struct node*)malloc(sizeof(struct node));
+        newNode->data = ele;
+        newNode->left = newNode->right = NULL;
+        return newNode;
+    }
+    if (ele < root->data) 
+        root->left = insert(root->left, ele);
     else if (ele > root->data) 
-        root->right = insert(root->right, ele);        
+        root->right = insert(root->right, ele);
     return root; 
 }
 
-void inorder(struct node *root) 
-{ 
-    if (root != NULL) 
-    { 
-        inorder(root->left); 
-        printf("%d \n", root->data); 
-        inorder(root->right); 
-    } 
-} 
-
-void preorder(struct node *root) 
-{ 
-    if (root != NULL) 
-    { 
-      printf("%d \n", root->data);
-        preorder(root->left);  
-        preorder(root->right); 
-    } 
-} 
-
-void postorder(struct node *root) 
-{ 
-    if (root != NULL) 
-    { 
-        postorder(root->left);  
-        postorder(root->right); 
-        printf("%d \n", root->data);
-    } 
-}
-
-int search(struct node *root)
-{
-  int ele;
-  printf("\nEnter the element to be searched: ");
-  scanf("%d", &ele);
-  while(root != NULL)
-  {
-    if(ele > root->data)
-      root = root->right;
-    else if(ele < root->data)
-      root = root->left;
-    else
-      return 1;
-  }
-  return 0;
-}
-
-int main()
-{
-  int opt, opt1, opt2, ele;
-  do
-  {
-    printf("\nBST Menu: \n1.Insert \n2.Traversal \n3.Search \n");
-    printf("\nEnter your choice: ");
-    scanf("%d", &opt1);
-    switch(opt1)
-    {
-      case 1:
-        printf("\nEnter the data of the node: ");
-        scanf("%d", &ele);
-        root = insert(root, ele);
-        break;
-      case 2:
-        printf("\nTraversal Menu: \n1.In-order \n2.Pre-order \n3.Post-order \n");
-        printf("\nEnter your choice: ");
-        scanf("%d", &opt2); 
-        switch(opt2)
-        {
-          case 1: 
-            printf("\nIn-order traversal: \n");
-            inorder(root);
-            break;
-          case 2:
-            printf("\nPre-order traversal: \n");
-            preorder(root);
-            break;
-          case 3:
-            printf("\nPost-order traversal: \n");
-            postorder(root);
-            break;
-          default:
-            printf("\nEnter valid choice.");
-        }
-        break;
-      case 3:
-        if(search(root) == 1)
-          printf("\nElement found.\n");
-        else
-          printf("\nElement not found.\n");
-        break;
-      default:
-        printf("Enter valid choice.\n");
+void traverse(struct node *root, void (*func)(struct node *)) {
+    if (root) {
+        func(root);
+        traverse(root->left, func);
+        traverse(root->right, func);
     }
-  printf("\nDo you want to continue? Press 1 to continue, 0 to exit: ");
-  scanf("%d", &opt);  
-  }while(opt!=0);
-  return 0;
+}
+
+void inorder(struct node *root) { printf("%d\n", root->data); }
+void preorder(struct node *root) { printf("%d\n", root->data); }
+void postorder(struct node *root) { printf("%d\n", root->data); }
+
+bool search(struct node *root, int ele) {
+    while (root) {
+        if (ele == root->data) return true;
+        root = (ele < root->data) ? root->left : root->right;
+    }
+    return false;
+}
+
+int main() {
+    struct node *root = NULL;
+    int opt, ele;
+
+    do {
+        printf("\nBST Menu: \n1. Insert \n2. Traversal \n3. Search \n");
+        printf("Enter your choice: ");
+        scanf("%d", &opt);
+
+        switch (opt) {
+            case 1:
+                printf("Enter the data of the node: ");
+                scanf("%d", &ele);
+                root = insert(root, ele);
+                break;
+            case 2: {
+                int traversalOpt;
+                printf("Traversal Menu: \n1. In-order \n2. Pre-order \n3. Post-order \n");
+                printf("Enter your choice: ");
+                scanf("%d", &traversalOpt);
+                switch (traversalOpt) {
+                    case 1: printf("In-order traversal:\n"); traverse(root, inorder); break;
+                    case 2: printf("Pre-order traversal:\n"); traverse(root, preorder); break;
+                    case 3: printf("Post-order traversal:\n"); traverse(root, postorder); break;
+                    default: printf("Enter valid choice.\n");
+                }
+                break;
+            }
+            case 3:
+                printf("Enter the element to be searched: ");
+                scanf("%d", &ele);
+                printf("Element %sfound.\n", search(root, ele) ? "" : "not ");
+                break;
+            default:
+                printf("Enter valid choice.\n");
+        }
+    } while (opt != 0);
+
+    return 0;
 }
